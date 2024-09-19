77a885c265e7c689103a48a52bb28b751051cb3f
RiverSideSky <tnals4637@naver.com>
2024-09-19 17:55:47 +0900

    9/19

--- src/02/MyClockImage.js
+++ src/02/MyClockImage.js
@@ -1,7 +1,8 @@
 function MyClockImage () {
     return(
         <>
-            <img src="colock.png" alt="시계이미지" />
+            <img src="colock.png" className="w-1/5" alt="시계이미지" />
+            <br/>
             <br/>
         </>
     );
